2fa65fa2f2882507bcd7c01242b4ff0184718ade
Adriana <adriana.be@outlook.com>
2018-10-31 16:41:15 -0600

    pintando burger

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,8 +4,8 @@ import classes from './Layout.css'
 
 const layout = (props) => (
   <Aux>
-    <div>Toolbar, slidebar, Backdrop</div>
-    <main className={classes.Container}>
+    <div>Toolbar, SideDrawer, Backdrop</div>
+    <main className={classes.Content}>
       {props.children}
     </main>
   </Aux>
